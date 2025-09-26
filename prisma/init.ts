9e3830483295b68668ab678aebcf64e533e3e787
Punay kukreja <kukrejapunay04@gmail.com>
2025-09-27 02:00:18 +0530

    fixed the errors

--- prisma/init.ts
+++ prisma/init.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@/app/generated/prisma";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"], // optional
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
