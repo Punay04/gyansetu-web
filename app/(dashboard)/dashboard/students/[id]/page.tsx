916c2849d847905d29d2e5ea99d063a7b321f053
Punay kukreja <kukrejapunay04@gmail.com>
2025-09-27 02:21:40 +0530

    fixed the errors

--- app/(dashboard)/dashboard/students/[id]/page.tsx
+++ app/(dashboard)/dashboard/students/[id]/page.tsx
@@ -1,13 +1,25 @@
 import StudentAnalyticsPage from "@/modals/dashboard/views/students-analytics-page";
 import React from "react";
+import { Metadata } from "next";
 
-const Page = ({ params }: { params: { id: string } }) => {
+interface PageProps {
+  params: {
+    id: string;
+  };
+  searchParams: Record<string, string | string[] | undefined>;
+}
+
+export const generateMetadata = ({ params }: PageProps): Metadata => {
+  return {
+    title: `Student ${params.id} - Analytics`,
+  };
+};
+
+export default function Page({ params }: PageProps) {
   const id = params.id;
   return (
     <div>
       <StudentAnalyticsPage id={id} />
     </div>
   );
-};
-
-export default Page;
+}
